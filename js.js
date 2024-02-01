ed18a837aa1b3724c1471584dc684244cefeebb2
Aleksandar Ilanković <122573605+SpicyUber@users.noreply.github.com>
2024-02-01 23:26:29 +0100

    Changes to js

--- js.js
+++ js.js
@@ -2,12 +2,25 @@ function hideThat(){
     var davideo= document.getElementById("myvideo");
     var dasource= document.getElementById("masource");
     var button= document.getElementById("mybutton");
-    if(davideo.paused){button.innerHTML='<i class="fa fa-play"></i>';}
-    else{ button.innerHTML='<i class="fa fa-pause"></i>';}}
+    if(davideo.paused){button.innerHTML='<i class="fa fa-rotate-right"></i>';button.style.opacity=1;}
+    else{ button.innerHTML='<i class="fa fa-play"></i>';}}
 function hideThatS(){
    var davideo= document.getElementById("myvideo");
    var dasource= document.getElementById("masource");
    var button= document.getElementById("mybutton");
-   if(davideo.paused){davideo.play();}
-   else{ davideo.pause();}
+   if(davideo.paused){davideo.play();fadeOutEffect();}
+}
+function fadeOutEffect(){
+    var fadeTarget = document.getElementById("mybutton");
+        var fadeEffect = setInterval(function () {
+            if (!fadeTarget.style.opacity) {
+                fadeTarget.style.opacity = 1;
+            }
+            if (fadeTarget.style.opacity > 0) {
+                fadeTarget.style.opacity -= 0.2;
+            } else {
+                clearInterval(fadeEffect);
+            }
+        }, 200);
+    
 }
